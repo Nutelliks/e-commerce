5185131d599980847e39a496f96eaf0fc4b82d8c
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-04-09 15:36:58 +0500

    custom User model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    delivery_address = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = 'users'
+        managed = True
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
