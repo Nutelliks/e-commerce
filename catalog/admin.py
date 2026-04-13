ccf1c1d877834a8534152919e858e2a5a7654e00
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-04-13 12:07:46 +0500

    catalog admin refactored

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,11 +11,12 @@ class CategoryInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "parent")
-    list_filter = ("parent",)
+    list_display = ("name", "slug", "parent", "is_active", "created_at")
+    list_filter = ("is_active", "parent", "created_at")
     search_fields = ("name", "slug")
     prepopulated_fields = {"slug": ("name",)}
     ordering = ("name",)
+    list_editable = ("is_active", )
     inlines = (CategoryInline,)
 
 
@@ -26,7 +27,7 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ("name", "slug", "description")
     prepopulated_fields = {"slug": ("name",)}
     list_editable = ("price", "stock")
-    readonly_fields = ("created_at",)
+    readonly_fields = ("created_at", "updated_at")
     fieldsets = (
         ("Main info", {"fields": ("name", "slug", "category", )}),
         (
@@ -35,4 +36,5 @@ class ProductAdmin(admin.ModelAdmin):
         ),
         ("Available", {"fields": ("price", "stock", "is_available")}),
         (None, {"fields": ("image",)}),
+        ("Meta", {"fields": ("created_at", "updated_at")}),
     )
