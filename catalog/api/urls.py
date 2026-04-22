f5482beb89db6137fda5d61eab1f1857fc2747cf
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-04-22 09:09:30 +0500

    fix(filters): improve price validation in ProductFilter

--- catalog/api/urls.py
+++ catalog/api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from .views import CategoryViewSet, ProductViewSet
 
+
 router = routers.SimpleRouter()
 router.register(r"categories", CategoryViewSet)
 router.register(r"products", ProductViewSet)
