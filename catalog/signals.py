077c37c03f8a0259ce302ae8b6ffb8ed9aa2ea8a
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-04-23 17:10:17 +0500

    feat(products): add cache invalidation

--- catalog/signals.py
+++ catalog/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.cache import cache
-from .models import Category
+from .models import Category, Product
 
 
 @receiver([post_delete, post_save], sender=Category)
@@ -10,3 +10,9 @@ def delete_categories_cache(sender, instance, **kwargs):
     cache.delete(f"categories:detail:{instance.slug}")
     if instance.parent is not None:
         cache.delete(f"categories:detail:{instance.parent.slug}")
+
+
+@receiver([post_delete, post_save], sender=Product)
+def delete_products_cache(sender, instance, **kwargs):
+    cache.delete_pattern("products:list:*")
+    cache.delete(f"products:detail:{instance.slug}")
