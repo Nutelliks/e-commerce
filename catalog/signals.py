f9bc68d7b9ec2fabf42ad43fa12331a7063c7297
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-02 06:40:55 +0500

    feat(catalog): logging for category and product

--- catalog/signals.py
+++ catalog/signals.py
@@ -3,9 +3,14 @@ from django.dispatch import receiver
 from django.core.cache import cache
 from .models import Category, Product
 
+import logging
+
+catalog_logger = logging.getLogger(__name__)
+
 
 @receiver([post_delete, post_save], sender=Category)
 def delete_categories_cache(sender, instance, **kwargs):
+    catalog_logger.info(f"Набор каталогов изменен. ID: {instance.id}")
     cache.delete("categories:list")
     cache.delete(f"categories:detail:{instance.slug}")
     if instance.parent is not None:
@@ -14,5 +19,6 @@ def delete_categories_cache(sender, instance, **kwargs):
 
 @receiver([post_delete, post_save], sender=Product)
 def delete_products_cache(sender, instance, **kwargs):
+    catalog_logger.info(f"Набор товаров изменен. ID: {instance.id}")
     cache.delete_pattern("products:list:*")
     cache.delete(f"products:detail:{instance.slug}")
