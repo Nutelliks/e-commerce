80dd78f4e0dfdc5872c1629b9c43c75ad12f4da4
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-03 06:19:47 +0500

    feat(carts): add CartItem model

--- carts/models.py
+++ carts/models.py
@@ -8,7 +8,20 @@ class Cart(BaseModel):
     session_key = models.CharField(max_length=40, unique=True, blank=True, null=True)
 
     class Meta:
-        db_table = 'carts'
+        db_table = "carts"
         managed = True
-        verbose_name = 'Cart'
-        verbose_name_plural = 'Carts'
+        verbose_name = "Cart"
+        verbose_name_plural = "Carts"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        to="cart.Carts", on_delete=models.CASCADE, related_name="cart_items"
+    )
+    product = models.ForeignKey(
+        to="catalog.Products", on_delete=models.SET_NULL, null=True
+    )
+    quantity = models.PositiveSmallIntegerField(default=1, verbose_name="Количество")
+    price_snapshot = models.DecimalField(
+        max_digits=7, decimal_places=2, verbose_name="Зафиксированная цена"
+    )
