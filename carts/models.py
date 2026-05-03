efd65bffbb113ddd14435b882989c871e1c8f3db
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-03 06:13:13 +0500

    feat(carts): implement Cart model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Cart(BaseModel):
+    user = models.OneToOneField(to="users.User", blank=True, null=True)
+    session_key = models.CharField(max_length=40, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'carts'
+        managed = True
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
