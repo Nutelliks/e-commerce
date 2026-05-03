f700c00d13276b38aab159ab04fe183065e2e5c9
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-03 07:13:11 +0500

    feat: middleware for session_key (get/create)

--- app/settings.py
+++ app/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'core.middleware.AnonymousSessionMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
---
+++ core/middleware.py
@@ -0,0 +1,11 @@
+class AnonymousSessionMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not request.session.session_key:
+            request.session.create()
+
+        response = self.get_response(request)
+
+        return response
