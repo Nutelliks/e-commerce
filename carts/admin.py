90e221e303989eea673b5e5e8ea3bdf52d9d7d79
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-04 05:33:05 +0500

    feat(carts): simple admin for cart models

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "session_key", "created_at", "updated_at")
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("cart", "product", "quantity", "price_snapshot")
+    readonly_fields = ("price_snapshot", )
