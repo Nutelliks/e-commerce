58bc91fc7666bdcfb278e86d41756f460fcbe02b
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-10 16:16:35 +0500

    feat(auth): add JWT auth conf

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = "users"
-urlpatterns = []
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
