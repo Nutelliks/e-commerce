f82834ed96a1dc44c9208adcde2413a30d70e1e9
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-11 16:02:04 +0500

    feat(users): add user registration endpoint

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -4,7 +4,7 @@ from ..models import User
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_oтly=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .api.views import UserRegisterAPIView
 
 app_name = "users"
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', UserRegisterAPIView.as_view(), name='register'),
 ]
