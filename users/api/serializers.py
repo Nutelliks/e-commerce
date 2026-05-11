5aeffff67ba3fb0000419e4ae96596fde71fc9d3
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-11 15:31:49 +0500

    feat(users): serializer for registration api

---
+++ users/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from ..models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_oтly=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password2")
+
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError("Passwords do not match")
+
+        return attrs
+
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+
+        return user
