22a7a496b5f775be2737197ff79d897ffb280fe7
Nurislam Audangaliev <naudangaliev@gmail.com>
2026-05-16 15:36:28 +0500

    feat(promocodes): admin interface for model

--- promocodes/admin.py
+++ promocodes/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import PromoCode
 
-# Register your models here.
+
+@admin.register(PromoCode)
+class PromoCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "discount_type",
+        "discount_value",
+        "max_uses",
+        "used_count",
+        "is_active",
+        "valid_from",
+        "valid_to",
+    )
+    list_filter = (
+        "is_active",
+        "discount_type",
+        "once_per_user",
+        "valid_from",
+        "valid_to",
+    )
+    ordering = ("-id",)
+    search_fields = ("code",)
+    readonly_fields = ("used_count",)
+    filter_horizontal = ("users",)
